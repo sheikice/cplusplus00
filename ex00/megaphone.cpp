2175ea5a0d02ad08988c36fc08bb548e0971847a
skc <sheikouz@gmail.com>
2025-09-08 17:28:10 +0200

    refacto megaphone.cpp

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,31 +6,27 @@
 /*   By: jwuille <jwuille@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/30 18:54:31 by jwuille           #+#    #+#             */
-/*   Updated: 2025/08/30 18:54:33 by jwuille          ###   ########.fr       */
+/*   Updated: 2025/09/08 17:25:14 by jwuille          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+using namespace std;
 
 int	main(int ac, char **av)
 {
-	int	i;
-	int	j;
-
 	if (ac == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
 	else
 	{
-		i = 0;
-		while (av[++i])
+		for (int i = 1; i < ac; i++)
 		{
-			j = -1;
-			while (av[i][++j])
-				std::cout << (char)toupper(av[i][j]);
+			for (int j = 0; av[i][j] != '\0'; j++)
+				cout << (char)toupper(av[i][j]);
 			if (av[i + 1])
-				std::cout << " ";
+				cout << " ";
 		}
-		std::cout << std::endl;
+		cout << endl;
 	}
 	return (0);
 }
