a90d6b9e1edd1690487bc6117ad266520f18a1d4
skc <sheikouz@gmail.com>
2025-09-13 18:12:29 +0200

    feat(PhoneBook.hpp)

---
+++ ex01/PhoneBook.hpp
@@ -0,0 +1,32 @@
+#ifndef PHONEBOOK_HPP
+# define PHONEBOOK_HPP
+
+# include <iostream>
+# include <sstream>
+# include "Contact.hpp"
+using namespace std;
+
+# define MAX_CONTACT 8
+# define CONTACT_SELECT_REQUEST "\nCHOSE INDEX: ";
+# define FIRST_REQUEST "\nADD, SEARCH or EXIT: "
+# define EMPTY ""
+# define NEW_LINE '\n'
+# define EXIT "EXIT"
+# define ADD "ADD"
+
+class	PhoneBook
+{
+	private:
+		int		contact_number;
+		void	contact_reorganise(void);
+		void	previsualisation(void);
+
+	public:
+		PhoneBook(void);
+		~PhoneBook(void);
+		Contact	contacts[MAX_CONTACT];
+		void	add(void);
+		void	search(void);
+};
+
+#endif
