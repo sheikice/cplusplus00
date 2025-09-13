e488fd6fd434db9f448ca13477522e52c9256644
skc <sheikouz@gmail.com>
2025-09-13 18:12:40 +0200

    feat(contact.cpp)

---
+++ ex01/Contact.cpp
@@ -0,0 +1,69 @@
+#include "Contact.hpp"
+
+Contact::Contact(void)
+{
+	this->first_name = "";
+	this->last_name = "";
+	this->nickname = "";
+	this->phone_number = "";
+	this->darkest_secret = "";
+	return ;
+}
+
+Contact::~Contact(void)
+{
+	return ;
+}
+
+void	Contact::arg_setter(string request, string& arg)
+{
+	while (cin && arg == "")
+	{
+		cout << request;
+		getline(cin, arg, '\n');
+	}
+}
+
+void	Contact::data_setter(void)
+{
+	arg_setter(FIRST_NAME, this->first_name);
+	arg_setter(LAST_NAME, this->last_name);
+	arg_setter(NICKNAME, this->nickname);
+	arg_setter(PHONE_NUMBER, this->phone_number);
+	arg_setter(DARKEST_SECRET, this->darkest_secret);
+}
+
+void	Contact::field_previsu(int index)
+{
+	cout << setw(10) << index << "|";
+}
+
+void	Contact::field_previsu(string arg)
+{
+	if (arg.length() <= 10)
+		cout << setw(10) << arg << "|";
+	else
+	{
+		arg.resize(9);
+		cout << setw(9) << arg << ".|";
+	}
+}
+
+void	Contact::contact_previsu(int index)
+{
+	field_previsu(index);
+	field_previsu(first_name);
+	field_previsu(last_name);
+	field_previsu(nickname);
+	cout << endl;
+	return ;
+}
+
+void	Contact::show_contact_info(void)
+{
+	cout << "\nfirst name: " << this->first_name << "\n";
+	cout << "last name: " << this->last_name << "\n";
+	cout << "nickname: " << this->nickname << "\n";
+	cout << "phone number: " << this->phone_number << "\n";
+	cout << "darkest secret: " << this->darkest_secret << endl;
+}
