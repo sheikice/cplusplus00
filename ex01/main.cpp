0daa964b4af8f6986dfd4350e646d05456a39562
skc <sheikouz@gmail.com>
2025-09-13 18:13:01 +0200

    feat(main)

---
+++ ex01/main.cpp
@@ -0,0 +1,18 @@
+#include "PhoneBook.hpp"
+
+int	main()
+{
+	PhoneBook	repo;
+	string		line;
+	while(cin && line != EXIT)
+	{
+		cout << FIRST_REQUEST;
+		getline(cin, line = EMPTY, NEW_LINE);
+		if (line == ADD)
+			repo.add();
+		if (line == "SEARCH")
+			repo.search();
+	}
+	cout << endl;
+	return (0);
+}
