2db46a3437a9c98cc439323d59ba9a8475346a9c
Jeremy Wuille <jwuille@f6r2s7.clusters.42paris.fr>
2025-11-03 16:41:20 +0100

    [fix] - Delete namespace

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,27 +6,26 @@
 /*   By: jwuille <jwuille@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/30 18:54:31 by jwuille           #+#    #+#             */
-/*   Updated: 2025/09/08 17:25:14 by jwuille          ###   ########.fr       */
+/*   Updated: 2025/11/03 16:09:53 by jwuille          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-using namespace std;
 
 int	main(int ac, char **av)
 {
 	if (ac == 1)
-		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
 	else
 	{
 		for (int i = 1; i < ac; i++)
 		{
 			for (int j = 0; av[i][j] != '\0'; j++)
-				cout << (char)toupper(av[i][j]);
+				std::cout << (char)toupper(av[i][j]);
 			if (av[i + 1])
-				cout << " ";
+				std::cout << " ";
 		}
-		cout << endl;
+		std::cout << std::endl;
 	}
 	return (0);
 }
