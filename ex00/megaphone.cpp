61eb163949ad4d22e1b8a8ea3ccc724c99005c47
skc <sheikouz@gmail.com>
2025-11-05 17:14:51 +0100

    [fix] - shorter

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: jwuille <jwuille@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/30 18:54:31 by jwuille           #+#    #+#             */
-/*   Updated: 2025/11/03 16:09:53 by jwuille          ###   ########.fr       */
+/*   Updated: 2025/11/09 13:59:05 by jwuille          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,11 @@
 int	main(int ac, char **av)
 {
 	if (ac == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	else
-	{
 		for (int i = 1; i < ac; i++)
-		{
 			for (int j = 0; av[i][j] != '\0'; j++)
-				std::cout << (char)toupper(av[i][j]);
-			if (av[i + 1])
-				std::cout << " ";
-		}
-		std::cout << std::endl;
-	}
+				std::cout << (char)std::toupper(av[i][j]);
+	std::cout << std::endl;
 	return (0);
 }
